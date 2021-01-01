d0a809f77ad15710a02fa2da7bf0ae9c2bd8337c
Alexander <alexander@Alex.true.true>
2021-01-01 18:54:15 +0700

    added video

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-
+/*!
+    * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+    */
     (function($) {
-  "use strict";
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -14,17 +20,18 @@
     }
   });
 
-
+  // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-
+  // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
     offset: 75
   });
 
+  // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-scrolled");
@@ -32,10 +39,12 @@
       $("#mainNav").removeClass("navbar-scrolled");
     }
   };
+  // Collapse now if page is not at top
   navbarCollapse();
+  // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-
+  // Magnific popup calls
   $('#portfolio').magnificPopup({
     delegate: 'a',
     type: 'image',
@@ -51,107 +60,4 @@
     }
   });
 
-})(jQuery);
-
-
-
-
-
-let countdown;
-  const timerDisplay = document.querySelector('.display_time_left');
-  const buttons = document.querySelectorAll('[data-time');
-
-
-  function timer(seconds) {
-    clearInterval(countdown);
-    const now = Date.now();
-    const then = now + seconds * 1000;
-    displayTimeLeft(seconds);
-    countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-      if(secondsLeft < 0) {
-      clearInterval(countdown);
-      return;
-      }
-      displayTimeLeft(secondsLeft);
-    }, 1000);
-  }
-
-    function displayTimeLeft(seconds) {
-      const minutes = Math.floor(seconds / 60);
-      const remainderSeconds = seconds % 60;
-      const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
-      document.title = display;
-      timerDisplay.textContent = display;
-    }
-
-
-   function startTimer() {
-    const seconds = parseInt(this.dataset.time);
-    countDownMusic()
-    timer(10);
-      setTimeout(function() {
-      showStopButton()
-      timer(seconds)
-    }, 10000);
-  }
-
-
-    buttons.forEach(button => button.addEventListener('click', startTimer));
-    // document.customForm.addEventListener('submit', function(e) {
-    //   e.preventDefault();
-    //   const mins = this.minutes.value;
-    //   timer(mins * 60);
-    //   this.reset();
-    // })
-
-
-    function stop() {
-      document.getElementById("stopButton").style.display = "none";
-      clearInterval(countdown);
-    }
-
-    function showTimerControls() {
-      document.getElementById("timerButtons").style.display = "block";
-    }
-    function hideTimerControls() {
-      document.getElementById("timerButtons").style.display = "none";
-    }
-
-    function hidePlayButton() {
-      document.getElementById("playButton").style.display = "none";
-    }
-
-    function showPlayButton() {
-      document.getElementById("playButton").style.display = "block";
-    }
-
-    function hideStopButton() {
-      document.getElementById("stopButton").style.display = "none";
-    }
-
-    function showStopButton() {
-      document.getElementById("stopButton").style.display = "block";
-    }
-
-
-    const countdownMusic = new Audio('countdown.mp3');
-    function countDownMusic() {
-      countdownMusic.play()
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})(jQuery); // End of use strict
